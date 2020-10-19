e0d199b147c5e494c94cecb5de887040d9423d3a
Francisco Perez <efperez90@gmail.com>
2020-10-19 17:34:32 -0500

    merge con cambios de yadira en pruebas

--- Desktop/Proyecto/proyect/src/Components/Alumnos.jsx
+++ Desktop/Proyecto/proyect/src/Components/Alumnos.jsx
@@ -4,26 +4,31 @@ import student from "../imagenes/estudiante.png";
 
 import { Link } from "react-router-dom";
 
-const Alumnos = () => {
+const Alumno = () => {
   return (
-    <div className="Task">
+    <div className="Alumno">
       <div>
-        <h1 className="informacion como">Como solucionar mis tareas en 6 pasos</h1>
-        <div className="lista">
-          <li className="profe-p">Selecciono mi tarea</li>
-          <li className="profe-p">Lleno el formulario</li>
-          <li className="profe-p">Envio mi solicitud</li>
-          <li className="profe-p">Evaluo las diferentes opciones</li>
-          <li className="profe-p">Selecciono lo que se ajusta a mi bolsillo y a mi nivel educativo</li>
-          <li className="profe-p">Entrego mi tarea y disfruto de mi tiempo libre</li>
-        </div>
         <div className="div-student">
+          <h1 className="informacion-como">¿Cómo solucionar mis tareas en 6 pasos?</h1>
+        </div>
+        <div className="Alumno-Imag">
           <img src={student} alt="student" className="student" />
+        </div>
+        <div className="lista">
+          <div>
+            <li className="profe-p"> <span> Selecciono mi tarea.</span></li>
+            <li className="profe-p"> <span> Lleno el formulario.</span></li>
+            <li className="profe-p"> <span> Envío mi solicitud.</span></li>
+            <li className="profe-p"> <span> Evalúo las diferentes opciones</span></li>
+            <li className="profe-p"> <span> Selecciono la que se ajusta a mi bolsillo.</span></li>
+            <li className="profe-p"> <span> Entrego mi tarea y disfruto de mi tiempo libre.</span></li>
+          </div>
+
         </div>
 
         <div className="Button-div">
           <Link to="/login">
-            <button className="Button-Task">INGRESAR</button>
+            <button className="Button-Alumno">INGRESAR</button>
           </Link>
         </div>
       
@@ -32,4 +37,4 @@ const Alumnos = () => {
   );
 };
 
-export default Alumnos;
+export default Alumno;
